7be6ad328807a8760c1df0366747cf937fbaf70d
DevDive <valentine92exp@gmail.com>
2024-12-08 19:57:07 +0900

    메모리 누수 감소, 재시도 코드 수정

--- Assets/Script/Manager/AdManager.cs
+++ Assets/Script/Manager/AdManager.cs
@@ -6,7 +6,7 @@ using GoogleMobileAds.Api;
 //using GoogleMobileAds.Common;
 public class AdManager : MonoBehaviour
 {
-    public static AdManager Instance {get; private set;}
+    public static AdManager Instance { get; private set; }
     //inspector에서 편집 가능하게
     public Dictionary<string, string> adUnitIds = new Dictionary<string, string>();
     public Dictionary<string, string> adInterUnitIds = new Dictionary<string, string>();
@@ -17,40 +17,41 @@ public class AdManager : MonoBehaviour
     public int GameCount = 0;
 
     public void Awake()
-	{
-		if(Instance != null && Instance != this)
-		{
-			Destroy(this); //새로 생긴걸 파괴
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this); //새로 생긴걸 파괴
             print("Destroy new AdManager");
-			return;
-		}
+            return;
+        }
         else
         {
-		    Instance = this; //this는 현재 Instance를 가리킴.
+            Instance = this; //this는 현재 Instance를 가리킴.
         }
-		DontDestroyOnLoad(gameObject); //씬이 로딩, 언로딩될 때도 메모리에 기억됨.
+        DontDestroyOnLoad(gameObject); //씬이 로딩, 언로딩될 때도 메모리에 기억됨.
         setRewards();
 
-        GameCount = PlayerPrefs.GetInt("GameCount",0);
+        GameCount = PlayerPrefs.GetInt("GameCount", 0);
     }
 
-    void OnEnable(){
-        GameManager.GameSet+=AdSet;
+    void OnEnable()
+    {
+        GameManager.GameSet += AdSet;
     }
-    
+
     public void setRewards()
     {
         MobileAds.Initialize(initStatus => { });
 
-        #if UNITY_ANDROID
-            this.adUnitIds.Add("coin","ca-app-pub-7387591250664253/3052509528");
-            this.adUnitIds.Add("continue","ca-app-pub-7387591250664253/6837015323");
-            this.adUnitIds.Add("coinStart","ca-app-pub-7387591250664253/5149973062");
-            this.adUnitIds.Add("time","ca-app-pub-7387591250664253/3038436771");
-            this.adUnitIds.Add("combo","ca-app-pub-7387591250664253/7721300702");
-            
-            this.adInterUnitIds.Add("10game", "ca-app-pub-7387591250664253/4537092958");
-        #elif UNITY_IOS
+#if UNITY_ANDROID
+        this.adUnitIds.Add("coin", "ca-app-pub-7387591250664253/3052509528");
+        this.adUnitIds.Add("continue", "ca-app-pub-7387591250664253/6837015323");
+        this.adUnitIds.Add("coinStart", "ca-app-pub-7387591250664253/5149973062");
+        // this.adUnitIds.Add("time","ca-app-pub-7387591250664253/3038436771");
+        // this.adUnitIds.Add("combo","ca-app-pub-7387591250664253/7721300702");
+
+        this.adInterUnitIds.Add("10game", "ca-app-pub-7387591250664253/4537092958");
+#elif UNITY_IOS
             this.adUnitIds.Add("coin","ca-app-pub-7387591250664253/4950840120");
             this.adUnitIds.Add("continue","ca-app-pub-7387591250664253/3721518930");
             this.adUnitIds.Add("coinStart","ca-app-pub-7387591250664253/2523809724");
@@ -58,29 +59,29 @@ public class AdManager : MonoBehaviour
             //this.adUnitIds.Add("combo","ca-app-pub-7387591250664253/7721300702");
 
             this.adInterUnitIds.Add("10game", "ca-app-pub-7387591250664253/6237917358");
-        #else
+#else
             //this.adUnitId.Add("coin","ca-app-pub-7387591250664253/3052509528");
             //this.adUnitId.Add("continue","ca-app-pub-7387591250664253/6837015323");
             //this.adUnitId.Add("time","ca-app-pub-7387591250664253/3038436771");
             //this.adUnitId.Add("combo","ca-app-pub-7387591250664253/7721300702");
-        #endif
+#endif
 
-        foreach(var key in adUnitIds.Keys)
+        foreach (var key in adUnitIds.Keys)
         {
-            if(!adUnitIds.ContainsKey(key))
+            if (!adUnitIds.ContainsKey(key))
             {
-                #if UNITY_ANDROID
-                    this.adUnitIds[key]="ca-app-pub-7387591250664253/3038436771";
-                #elif UNITY_IOS
+#if UNITY_ANDROID
+                this.adUnitIds[key] = "ca-app-pub-7387591250664253/3038436771";
+#elif UNITY_IOS
                     this.adUnitIds[key]="ca-app-pub-7387591250664253/4950840120";
-                #endif
+#endif
             }
-            this.rewardRequestCount.Add(key,0);
+            this.rewardRequestCount.Add(key, 0);
             this.rewardedAds.Add(key, CreateRewardedAd(key));
         }
 
-        foreach(KeyValuePair<string,string> entry in adInterUnitIds)
-        {   
+        foreach (KeyValuePair<string, string> entry in adInterUnitIds)
+        {
             this.interAds.Add(entry.Key, new InterstitialAd(entry.Value));
         }
 
@@ -92,17 +93,17 @@ public class AdManager : MonoBehaviour
         RewardedAd Ad = new RewardedAd(Id);
         //핸들러
         // Called when an ad request has successfully loaded.
-        Ad.OnAdLoaded += delegate(object sender, EventArgs args) {HandleRewardedAdLoaded(sender, args, adUnitName);};
+        Ad.OnAdLoaded += delegate (object sender, EventArgs args) { HandleRewardedAdLoaded(sender, args, adUnitName); };
         // Called when an ad request failed to load.
-        Ad.OnAdFailedToLoad += delegate(object sender, AdFailedToLoadEventArgs args) {HandleRewardedAdFailedToLoad(sender, args, adUnitName);};
+        Ad.OnAdFailedToLoad += delegate (object sender, AdFailedToLoadEventArgs args) { HandleRewardedAdFailedToLoad(sender, args, adUnitName); };
         // Called when an ad is shown.
         Ad.OnAdOpening += HandleRewardedAdOpening;
         // Called when an ad request failed to show.
         Ad.OnAdFailedToShow += HandleRewardedAdFailedToShow;
         // Called when the user should be rewarded for interacting with the ad.
-        Ad.OnUserEarnedReward += delegate(object sender, Reward args) {HandleUserEarnedReward(sender, args, adUnitName);};
+        Ad.OnUserEarnedReward += delegate (object sender, Reward args) { HandleUserEarnedReward(sender, args, adUnitName); };
         // Called when the ad is closed.
-        Ad.OnAdClosed += delegate(object sender, EventArgs e) {HandleRewardedAdClosed(adUnitName);};
+        Ad.OnAdClosed += delegate (object sender, EventArgs e) { HandleRewardedAdClosed(adUnitName); };
         return Ad;
     }
 
@@ -115,7 +116,7 @@ public class AdManager : MonoBehaviour
         Ad.OnAdFailedToLoad += HandleOnAdFailedToLoad;
         // Called when an ad is shown.
         Ad.OnAdOpening += HandleOnAdOpening;
-        Ad.OnAdClosed += delegate(object sender, EventArgs e) {HandleOnAdClosed(adUnitName);};
+        Ad.OnAdClosed += delegate (object sender, EventArgs e) { HandleOnAdClosed(adUnitName); };
         return Ad;
     }
 
@@ -159,11 +160,24 @@ public class AdManager : MonoBehaviour
     public void HandleRewardedAdFailedToLoad(object sender, AdFailedToLoadEventArgs args, string name)
     {
         print("HandleRewardedAdFailedToLoad event received with message: " + args.ToString());
-        if(this.rewardRequestCount[name]>3)
+
+        if (this.rewardRequestCount[name] >= 3)
         {
+            print(name + " Ad Max Retry Exceeded. Destroying and recreating the ad.");
+
+            if (this.rewardedAds.ContainsKey(name))
+            {
+                this.rewardedAds[name]?.Destroy();
+            }
+
             this.rewardedAds[name] = CreateAndLoadRewardedAd(name);
+            this.rewardRequestCount[name] = 1;
+        }
+        else
+        {
+            print(name + " Retry #" + (this.rewardRequestCount[name] + 1));
+            LoadRewardedAd(name);
             this.rewardRequestCount[name] += 1;
-            print(name+ " Ad Max Requested");
         }
     }
 
@@ -176,13 +190,17 @@ public class AdManager : MonoBehaviour
     {
         print(
             "HandleRewardedAdFailedToShow event received with message: "
-                             + args.AdError);  
+                             + args.AdError);
     }
 
     public void HandleRewardedAdClosed(string adUnitName)
     {
         print("HandleRewardedAdClosed event received");
         print(adUnitName);
+        if (this.rewardedAds.ContainsKey(adUnitName))
+        {
+            this.rewardedAds[adUnitName]?.Destroy();
+        }
         this.rewardedAds[adUnitName] = CreateAndLoadRewardedAd(adUnitName);
     }
 
@@ -194,23 +212,23 @@ public class AdManager : MonoBehaviour
             "HandleRewardedAdRewarded event received for "
                         + amount.ToString() + " " + type);
 
-        if(name=="coin")
+        if (name == "coin")
         {
             RewardManager.Instance.addRandomCoin();
         }
-        else if(name=="continue")
+        else if (name == "continue")
         {
             RewardManager.Instance.continueGame();
         }
-        else if(name=="coinStart")
+        else if (name == "coinStart")
         {
             RewardManager.Instance.coinGameStart();
         }
-        else if(name=="time")
-        {   
+        else if (name == "time")
+        {
             RewardManager.Instance.SetTimeBonus(10.0f);
         }
-        else if(name=="combo")
+        else if (name == "combo")
         {
             RewardManager.Instance.SetComboTimeBonus(1.0f);
         }
@@ -239,6 +257,10 @@ public class AdManager : MonoBehaviour
     {
         MonoBehaviour.print("HandleAdClosed event received");
         BGMManager.Instance.BGMOn();
+        if (this.interAds.ContainsKey(adUnitName))
+        {
+            this.interAds[adUnitName]?.Destroy();
+        }
         this.interAds[adUnitName] = CreateAndLoadInterAd(adUnitName);
     }
 
@@ -252,26 +274,35 @@ public class AdManager : MonoBehaviour
         else
         {
             print("NOT Loaded Rewarded");
+            if (this.rewardedAds.ContainsKey(name))
+            {
+                this.rewardedAds[name]?.Destroy();
+            }
             this.rewardedAds[name] = CreateAndLoadRewardedAd(name);
         }
     }
 
-    public void ShowInterAd(string name) {
+    public void ShowInterAd(string name)
+    {
         InterstitialAd Ad = this.interAds[name];
-        if(Ad.IsLoaded())
+        if (Ad.IsLoaded())
         {
             Ad.Show();
         }
         else
         {
             print("NOT Loaded Interstitial");
+            if (this.interAds.ContainsKey(name))
+            {
+                this.interAds[name]?.Destroy();
+            }
             this.interAds[name] = CreateAndLoadInterAd(name);
         }
     }
 
     void AdSet()
     {
-        if(GameCount >= GameToAdCount)
+        if (GameCount >= GameToAdCount)
         {
             LoadInterAd("10game");
             //startAd = restartButton.GetComponent<Btn_AlarmImage>();
@@ -279,21 +310,22 @@ public class AdManager : MonoBehaviour
             //게임코드 정리
         }
 
-        PlayerPrefs.SetInt("GameCount",GameCount++);
+        PlayerPrefs.SetInt("GameCount", GameCount++);
         Debug.Log("GameCount: " + GameCount);
 
-        if(!rewardedAds["continue"].IsLoaded()){
+        if (!rewardedAds["continue"].IsLoaded())
+        {
             LoadRewardedAd("continue");
         }
     }
 
     public void showEndGameAd()
     {
-        if(GameCount >= GameToAdCount && interAds["10game"].IsLoaded())
+        if (GameCount >= GameToAdCount && interAds["10game"].IsLoaded())
         {
             ShowInterAd("10game");
             GameCount = 0;
-            PlayerPrefs.SetInt("GameCount",GameCount); //setter로 변경
+            PlayerPrefs.SetInt("GameCount", GameCount); //setter로 변경
             RewardManager.Instance.addCoin(1);
         }
     }
